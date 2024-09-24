9423f6e411d38b35939d350b5014477947b1eec7
kyujenius <hkj020607@gmail.com>
2024-09-24 13:07:57 +0900

    ✨ feat: router 추가 설정

--- src/router.jsx
+++ src/router.jsx
@@ -3,12 +3,14 @@ import IntroPage from "./pages/intro/introPage";
 
 import NotFoundPage from "./pages/error/notFoundPage";
 import ChattingPage from "./pages/chat/chattingPage";
+import CustomizingPage from "./pages/customize/customizingPage";
 
 const AppRouter = () => (
   <Router>
     <Routes>
       <Route path="/" element={<IntroPage />} />
       <Route path="/chat" element={<ChattingPage />} />
+      <Route path="/customize" element={<CustomizingPage />} />
 
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
