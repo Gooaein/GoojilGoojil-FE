af2d69d20636bd6955630608c561532ac1d18e4e
kyujenius <hkj020607@gmail.com>
2024-09-25 02:30:07 +0900

    ♻️ refactor: 라우팅 변경

--- src/router.jsx
+++ src/router.jsx
@@ -2,16 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import IntroPage from "./pages/intro/introPage";
 
 import NotFoundPage from "./pages/error/notFoundPage";
-import ChattingPage from "./pages/chat/chattingPage";
 import CustomizingPage from "./pages/customize/customizingPage";
-import { Header } from "./components/common/Header";
+import { Header } from "./components/common/Header/Header";
+import ChattingRoomPage from "./pages/chat/chattingRoomPage";
 
 const AppRouter = () => (
   <Router>
     <Header />
     <Routes>
       <Route path="/" element={<IntroPage />} />
-      <Route path="/chat" element={<ChattingPage />} />
+      <Route path="/chattingRoom" element={<ChattingRoomPage />} />
       <Route path="/customize" element={<CustomizingPage />} />
 
       <Route path="*" element={<NotFoundPage />} />
