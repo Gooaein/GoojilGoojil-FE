36fb08bdaa3dd20a65d52664836c10654d23214f
kyujenius <hkj020607@gmail.com>
2024-09-24 13:08:25 +0900

    ✨ feat: waterDropCharacterCustomizing 페이지 제작

---
+++ src/pages/customize/customizingPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import WaterDropCharacterCustomizing from "../../components/water-drop/WaterDropCharacterCustomizing";
+
+const CustomizingPage = () => {
+  return (
+    <div>
+      <WaterDropCharacterCustomizing />
+    </div>
+  );
+};
+
+export default CustomizingPage;
