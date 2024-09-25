2f368c32a56f35b583f66e080a6b1a5192ef6bc1
kyujenius <hkj020607@gmail.com>
2024-09-25 17:15:59 +0900

    ✨ feat: 디버깅 설정

--- src/components/common/CloudComponent.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import cloud from '../../assets/images/cloud.png';
-const CloudComponent = () => {
-  return <img src={cloud} alt="Cloud" />;
-};
-export default CloudComponent;
