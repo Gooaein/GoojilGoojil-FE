4c4d1e381c0289fb67a90987d3967160a2c44fd2
kyujenius <hkj020607@gmail.com>
2024-09-26 15:38:46 +0900

    ♻️ refactor: 실시간 사용자 수 표시 페이지 리팩토링

--- src/components/common/Header/components/ActiveUsers.jsx
+++ src/components/common/Header/components/ActiveUsers.jsx
@@ -2,22 +2,32 @@ import React from "react";
 import { useRecoilValue } from "recoil";
 import { activeUsersState } from "../../../../recoil/chat-atoms";
 import styles from "./ActiveUsers.module.css";
+
 const ActiveUsers = () => {
   const activeUsers = useRecoilValue(activeUsersState);
 
+  if (activeUsers.length === 0) {
+    return (
+      <div className={styles.emptyState}>현재 활성 사용자가 없습니다.</div>
+    );
+  }
+
   return (
     <div className={styles.activeUsers}>
-      <div className={styles.avatarContainer}>
+      <div className={styles.avatarGrid}>
         {activeUsers.map((user) => (
-          <div key={user.guest_id} className={styles.avatar}>
+          <div key={user.id} className={styles.avatarItem}>
             <img
-              src={`data:image/png;base64,${user.avartar_base64}`}
-              alt={`User ${user.guest_id}`}
+              src={user.avatar}
+              alt={`${user.name}'s avatar`}
+              className={styles.avatar}
             />
+            <span className={styles.userName}>{user.name}</span>
           </div>
         ))}
       </div>
     </div>
   );
 };
+
 export default ActiveUsers;
