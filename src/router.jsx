98dd9e11c9358702d4f9b117a668f94c1e29b2a8
김지민 <jimin1945@naver.com>
2024-09-28 04:41:48 +0900

    ✨ feat: 로그인페이지 퍼블리싱

--- src/router.jsx
+++ src/router.jsx
@@ -6,6 +6,7 @@ import CustomizingPage from "./pages/customize/customizingPage";
 import { Header } from "./components/common/Header/Header";
 import ChattingRoomPage from "./pages/chat/chattingRoomPage";
 import SurveyPage from "./pages/survey/surveyPage";
+import LoginPage from "./pages/login/loginPage";
 
 const AppRouter = () => (
   <Router>
@@ -15,6 +16,7 @@ const AppRouter = () => (
       <Route path="/chattingRoom" element={<ChattingRoomPage />} />
       <Route path="/customize" element={<CustomizingPage />} />
       <Route path="/survey" element={<SurveyPage />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   </Router>
