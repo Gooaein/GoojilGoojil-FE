12376faa9592a63cd83caa4366f0a4e72ea541a1
kyujenius <hkj020607@gmail.com>
2024-09-29 00:35:46 +0900

    🐛 fix: StompProvider 위치 변경

--- src/router.jsx
+++ src/router.jsx
@@ -1,31 +1,42 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import IntroPage from "./pages/intro/introPage";
-
 import NotFoundPage from "./pages/error/notFoundPage";
 import CustomizingPage from "./pages/customize/customizingPage";
 import { Header } from "./components/common/Header/Header";
 import ChattingRoomPage from "./pages/chat/chattingRoomPage";
+import SpeakerRoomPage from "./pages/speakerRoom/speakerRoomPage";
 import SurveyPage from "./pages/survey/surveyPage";
 import LoginPage from "./pages/login/loginPage";
 import CreateRoomPage from "./pages/createRoom/createRoomPage";
 import RoomListPage from "./pages/list/roomListPage";
-import SpeakerRoomPage from "./pages/speakerRoom/speakerRoomPage";
 import { StompProvider } from "./context/StompContext";
+
+const StompWrappedChattingRoom = () => (
+  <StompProvider>
+    <ChattingRoomPage />
+  </StompProvider>
+);
+
+const StompWrappedSpeakerRoom = () => (
+  <StompProvider>
+    <SpeakerRoomPage />
+  </StompProvider>
+);
 
 const AppRouter = () => (
   <Router>
     <Header />
     <Routes>
       <Route path="/" element={<IntroPage />} />
-      <StompProvider>
-        <Route path="/chattingRoom" element={<ChattingRoomPage />} />
-        <Route path="/speakerRoom" element={<SpeakerRoomPage />} />
-      </StompProvider>
-
+      <Route path="/chattingRoom" element={<StompWrappedChattingRoom />} />
+      <Route path="/speakerRoom" element={<StompWrappedSpeakerRoom />} />
       <Route path="/customize" element={<CustomizingPage />} />
       <Route path="/survey" element={<SurveyPage />} />
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/:uuid/chattingRoom" element={<ChattingRoomPage />} />
+      <Route
+        path="/:uuid/chattingRoom"
+        element={<StompWrappedChattingRoom />}
+      />
       <Route path="/:uuid/customize" element={<CustomizingPage />} />
       <Route path="/createRoom" element={<CreateRoomPage />} />
       <Route path="/list" element={<RoomListPage />} />
